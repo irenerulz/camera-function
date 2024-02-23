ba1f8382e63d6f77b9735dd424a7ebd6049283c4
irenerulz <101012417+irenerulz@users.noreply.github.com>
2024-02-22 19:47:49 -0800

    Add files via upload
    Wow I hope this works if you just download it as zip and then run it and then its a camera app

---
+++ camera-function/camera-function/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  camera-function
+//
+//  Created by Irene Lee on 2/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var button: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView.backgroundColor = .secondarySystemBackground
+        
+        button.backgroundColor = .systemBlue
+        button.setTitle("Take Picture",
+                        for: .normal)
+        button.setTitleColor(.white,
+                             for: .normal)
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func didTapButton(){
+        let picker = UIImagePickerController()//picker can be named anything
+        if UIImagePickerController .isSourceTypeAvailable(UIImagePickerController.SourceType.camera)
+        {
+            picker.sourceType = .camera //.photolibrary for photolibrary
+            //picker.allowsEditing = true
+            picker.delegate = self
+            self .present(picker, animated: true, completion: nil)
+
+        }
+    
+        
+    }
+}
+
+extension ViewController: UIImagePickerControllerDelegate,
+    UINavigationControllerDelegate{
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        //picker.dismiss(animated:true, completion:nil)
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            self.imageView.contentMode = .scaleAspectFit
+            self.imageView.image = image
+            dismiss(animated:true)
+     
+        }
+        //imageView.image = image
+    }
+    
+}
